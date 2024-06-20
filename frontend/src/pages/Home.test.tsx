1f57dc997d02bc1f75b52d5ac3ce604d321ba35f
Samuel Gesang <sgesang@mailo.com>
2024-06-20 09:25:23 +0200

    Change node version in CI Code Cleanup

--- frontend/src/pages/Home.test.tsx
+++ frontend/src/pages/Home.test.tsx
@@ -27,89 +27,15 @@ describe('Home Component', () => {
 
     test('renders Home with SiteList', () => {
         render(<Home sites={sites}/>);
-
         const siteList = screen.getByText('Site 1');
         expect(siteList).toBeInTheDocument();
     });
 
-    // test('displays SiteForm when Add Site button is clicked', () => {
-    //     render(<Home sites={sites}/>);
-    //
-    //     // Ensure the Add Site button is in the document
-    //     const addButton = screen.getByRole('button', {name: /add site/i});
-    //     fireEvent.click(addButton);
-    //
-    //     const siteForm = screen.getByText('Add Site');
-    //     expect(siteForm).toBeInTheDocument();
-    // });
-    //
-    // test('calls handleAddSite when Add button in form is clicked', () => {
-    //     render(<Home sites={sites}/>);
-    //
-    //     const addButton = screen.getByRole('button', {name: /add site/i});
-    //     fireEvent.click(addButton);
-    //
-    //     const siteForm = screen.getByText('Add Site');
-    //     expect(siteForm).toBeInTheDocument();
-    //
-    //     // Simulate filling out and submitting the form
-    //     const titleInput = screen.getByLabelText('Title');
-    //     const urlInput = screen.getByLabelText('URL');
-    //     fireEvent.change(titleInput, {target: {value: 'New Site'}});
-    //     fireEvent.change(urlInput, {target: {value: 'https://newsite.com'}});
-    //
-    //     const saveButton = screen.getByRole('button', {name: /^add$/i});
-    //     fireEvent.click(saveButton);
-    //
-    //     // Check console log for added site
-    //     // You might want to spy on console.log or use a mock function
-    // });
-    //
-    // test('calls handleEditSite when Edit button is clicked', () => {
-    //     render(<Home sites={sites}/>);
-    //
-    //     const editButtons = screen.getAllByRole('button', {name: /edit site/i});
-    //     expect(editButtons.length).toBeGreaterThan(0);
-    //
-    //     const editButton = editButtons[0]; // Select the first "Edit Site" button
-    //     fireEvent.click(editButton);
-    //
-    //     const siteForm = screen.getByText('Edit Site');
-    //     expect(siteForm).toBeInTheDocument();
-    //
-    //     // Simulate filling out and submitting the form
-    //     const titleInput = screen.getByLabelText('Title');
-    //     const urlInput = screen.getByLabelText('URL');
-    //     fireEvent.change(titleInput, {target: {value: 'Updated Site'}});
-    //     fireEvent.change(urlInput, {target: {value: 'https://updatedsite.com'}});
-    //
-    //     const saveButton = screen.getByRole('button', {name: /^save$/i});
-    //     fireEvent.click(saveButton);
-    //
-    //     // Check console log for edited site
-    //     // You might want to spy on console.log or use a mock function
-    // });
-    //
-    // test('calls handleDeleteSite when Delete button is clicked', () => {
-    //     render(<Home sites={sites}/>);
-    //
-    //     const deleteButtons = screen.getAllByRole('button', {name: "Delete"});
-    //     expect(deleteButtons.length).toBeGreaterThan(0);
-    //
-    //     const deleteButton = deleteButtons[0];
-    //     fireEvent.click(deleteButton);
-    //
-    //     // Check console log for deleted site
-    //     // You might want to spy on console.log or use a mock function
-    // });
 
     test('calls handleCrawlAllSites when Crawl All button is clicked', () => {
         render(<Home sites={sites}/>);
 
         const crawlAllButton = screen.getByRole('button', {name: /crawl all/i});
         fireEvent.click(crawlAllButton);
-
-        // Check console log for crawl all sites
-        // You might want to spy on console.log or use a mock function
     });
 });
